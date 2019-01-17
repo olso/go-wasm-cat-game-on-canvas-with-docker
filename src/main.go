2bba076f399679a72f02b8b345098ae40b81515f
olso <olsansky.martin@gmail.com>
2019-01-17 17:46:35 +0100

    touch tap

--- src/main.go
+++ src/main.go
@@ -1,54 +1,70 @@
 package main
 
 import (
-	"runtime"
+	"fmt"
+	"reflect"
 	"syscall/js"
 	// "net/http"
 )
 
-type RedDotPosition struct {
-	X     float64
-	Y     float64
-	id    int
-	alive bool
-}
-
 var (
-	win        js.Value = js.Global()
-	doc        js.Value = win.Get("document")
-	body       js.Value = doc.Get("body")
-	canvas     js.Value
-	renderGame js.Func
+	win           js.Value = js.Global()
+	doc           js.Value = win.Get("document")
+	body          js.Value = doc.Get("body")
+	canvas        js.Value
+	renderer      js.Func
+	mousePosition Point
+	laserPosition Point
 )
 
 /*
 The js namespace is from https://golang.org/pkg/syscall/js
 */
-func game() {
+func main() {
+	runGameForever := make(chan bool) // https://stackoverflow.com/questions/47262088/golang-forever-channel
+	fmt.Println("0", reflect.TypeOf(renderer))
+
 	setupCanvas()
 
-	renderGame = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	renderer := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		// canvasCtx := canvas.Call("getContext", "2d")
 		// touchClickEventHandler := js.NewCallback(func(args []js.Value)) {
 		// }
 		// doc.Call("addEventListener", "click", touchClickEventHandler, false)
 		// doc.Call("addEventListener", "touchstart", false)
 		// http.Get("http://google.com")
-		// fmt.Println("Rendering!")
-		win.Call("requestAnimationFrame", renderGame)
-		return nil
+		fmt.Println("Rendering!")
+		fmt.Println("2", reflect.TypeOf(renderer))
+		win.Call("requestAnimationFrame", renderer)
+		return this
 	})
-
 	// postpones execution at the end; clean up memory
-	defer renderGame.Release()
+	defer renderer.Release()
 
-	// prefer browser repaint in next event loop, think 60fps anims
-	win.Call("requestAnimationFrame", renderGame)
+	fmt.Println("1", reflect.TypeOf(renderer))
+
+	// for the 60fps anims
+	win.Call("requestAnimationFrame", renderer)
+
+	// var mouseEventHandler js.Func = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	// 	// fmt.Println(this.Get("clientX").Float())
+	// 	// fmt.Println(e.Get("clientX").Float())
+	// 	fmt.Println("click tap")
+	// 	// mousePosition.x = e.Get("clientX").Float()
+	// 	// mousePosition.y = e.Get("clientY").Float()
+	// 	return this
+	// })
+	// defer mouseEventHandler.Release()
+	// win.Call("addEventListener", "click", mouseEventHandler, false)
+	// win.Call("addEventListener", "touchstart", mouseEventHandler, false)
+
+	<-runGameForever
 }
 
-func main() {
-	go game()        // Run game in goroutine, run forever
-	runtime.Goexit() // https://golang.org/pkg/runtime/#Goexit
+// Helpers
+type Point struct {
+	x float64
+	y float64
 }
 
 func setupCanvas() {
